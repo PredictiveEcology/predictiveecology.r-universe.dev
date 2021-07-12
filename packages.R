94f0d97f2554a99b35895613131a7c9343fb5673
Alex Chubaty <achubaty@for-cast.ca>
2021-07-12 12:40:44 -0600

    initial commit: script to create packages.json

---
+++ packages.R
@@ -0,0 +1,25 @@
+repos <- c(
+  "achubaty/amc",
+  "PredictiveEcology/CBMutils",
+  "PredictiveEcology/fireSenseUtils",
+  "PredictiveEcology/LandR",
+  "PredictiveEcology/LandWebUtils",
+  "PredictiveEcology/map",
+  "PredictiveEcology/pemisc",
+  "PredictiveEcology/peutils",
+  "PredictiveEcology/quickPlot",
+  "PredictiveEcology/Require",
+  "PredictiveEcology/reproducible",
+  "PredictiveEcology/SpaDES.core",
+  "PredictiveEcology/SpaDES.install",
+  "PredictiveEcology/SpaDES.tools",
+  "PredictiveEcology/usefulFuns"
+)
+gh_prefix <- "https://github.com/"
+
+df <- data.frame(
+  package = basename(repos),
+  url = paste0(gh_prefix, repos),
+  branch = "development"
+)
+jsonlite::write_json(df, 'packages.json', pretty = TRUE)
