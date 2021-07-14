2059ebb9349b1b94b7d40c5cb4bd9d22e6f97db9
Alex Chubaty <achubaty@for-cast.ca>
2021-07-14 13:31:15 -0600

    add some packages; use development branches for most

--- packages.R
+++ packages.R
@@ -1,26 +1,33 @@
 repos <- c(
-  "achubaty/amc",
-  "PredictiveEcology/CBMutils",
-  "PredictiveEcology/fireSenseUtils",
-  "PredictiveEcology/LandR",
-  "PredictiveEcology/LandWebUtils",
-  "PredictiveEcology/NetLogoR",
-  "PredictiveEcology/map",
-  "PredictiveEcology/pemisc",
-  "PredictiveEcology/peutils",
-  "PredictiveEcology/quickPlot",
-  "PredictiveEcology/Require",
-  "PredictiveEcology/reproducible",
-  "PredictiveEcology/SpaDES.core",
-  "PredictiveEcology/SpaDES.install",
-  "PredictiveEcology/SpaDES.tools",
-  "PredictiveEcology/usefulFuns"
+  "achubaty/amc@development",
+  "PredictiveEcology/CBMutils@development",
+  "PredictiveEcology/fireSenseUtils@development",
+  "PredictiveEcology/LandR@development",
+  "PredictiveEcology/LandWebUtils@development",
+  "PredictiveEcology/NetLogoR@development",
+  "PredictiveEcology/map@development",
+  "PredictiveEcology/pemisc@development",
+  "PredictiveEcology/peutils@development",
+  "PredictiveEcology/quickPlot@development",
+  "PredictiveEcology/Require@development",
+  "PredictiveEcology/reproducible@development",
+  "PredictiveEcology/SpaDES.addins@development",
+  "PredictiveEcology/SpaDES.core@development",
+  "PredictiveEcology/SpaDES.experiment@development",
+  "PredictiveEcology/SpaDES.install@development",
+  "PredictiveEcology/SpaDES.shiny@development",
+  "PredictiveEcology/SpaDES.tools@development",
+  "PredictiveEcology/usefulFuns@development",
+  "s-u/fastshp"
 )
+repos2 <- sapply(repos, function(x) strsplit(x, "@")[[1]][1])
+branches <- sapply(repos, function(x) strsplit(x, "@")[[1]][2])
 gh_prefix <- "https://github.com/"
 
 df <- data.frame(
-  package = basename(repos),
-  url = paste0(gh_prefix, repos)#,
-  #branch = "development"
+  package = basename(repos2),
+  url = paste0(gh_prefix, repos2),
+  branch = branches,
+  row.names = NULL
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
